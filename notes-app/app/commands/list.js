d39750b3999ec4e61162baef73d8500485936fbd
Nicolas Saavedra <njsr27@gmail.com>
2021-08-21 19:56:24 -0300

    Added 4 spaces instead of 2

--- notes-app/app/commands/list.js
+++ notes-app/app/commands/list.js
@@ -2,5 +2,5 @@ module.exports.command = "list";
 module.exports.describe = "Lists the notes";
 module.exports.builder = {};
 module.exports.handler = () => {
-  console.log("Listing note");
+    console.log("Listing note");
 };
--- notes-app/app/commands/remove.js
+++ notes-app/app/commands/remove.js
@@ -2,5 +2,5 @@ module.exports.command = "remove";
 module.exports.describe = "Removes a note";
 module.exports.builder = {};
 module.exports.handler = () => {
-  console.log("Removing note");
+    console.log("Removing note");
 };
