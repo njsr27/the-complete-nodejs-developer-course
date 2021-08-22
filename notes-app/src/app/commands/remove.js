dd09d6fe668704042a4995434ecc3a9db226301c
Nicolas Saavedra <njsr27@gmail.com>
2021-08-22 03:24:17 -0300

    Added remove functionality

--- notes-app/src/app/commands/remove.js
+++ notes-app/src/app/commands/remove.js
@@ -1,6 +1,30 @@
+const chalk = require("chalk");
+const io = require("../utils/io");
+
 module.exports.command = "remove";
 module.exports.describe = "Removes a note";
-module.exports.builder = {};
-module.exports.handler = () => {
-  console.log("Removing note");
+module.exports.builder = (yargs) => {
+  yargs.option("title", {
+    describe: "Title of the note",
+    type: "string",
+    demandOption: true,
+  });
+};
+module.exports.handler = (argv) => {
+  const notes = io.getNotes();
+
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].title === argv.title) {
+      console.log(
+        chalk.green("[INFO] ") +
+          "Removed note with title: " +
+          chalk.magenta(argv.title)
+      );
+      notes.splice(i, 1);
+      io.saveNotes(notes);
+      return;
+    }
+  }
+
+  console.log(chalk.red("[ERROR] ") + "Note with that title does not exist");
 };
