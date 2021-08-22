3e2b76246958e68a49385a284e63936aaba8c7a0
Nicolas Saavedra <njsr27@gmail.com>
2021-08-22 04:51:28 -0300

    Added list and read command

--- notes-app/src/app/commands/list.js
+++ notes-app/src/app/commands/list.js
@@ -1,6 +1,11 @@
+const io = require("../utils/io");
+const chalk = require("chalk");
 module.exports.command = "list";
 module.exports.describe = "Lists the notes";
 module.exports.builder = {};
 module.exports.handler = () => {
-  console.log("Listing note");
+  const notes = io.getNotes();
+  console.log(chalk.blue("=== Your notes ==="));
+  notes.forEach((note) => console.log(note.title));
+  console.log(chalk.blue("=================="));
 };
--- notes-app/src/app/commands/read.js
+++ notes-app/src/app/commands/read.js
@@ -1,6 +1,24 @@
+const io = require("../utils/io");
+const chalk = require("chalk");
 module.exports.command = "read";
 module.exports.describe = "Reads a note";
-module.exports.builder = {};
-module.exports.handler = () => {
-  console.log("Reading note");
+module.exports.builder = (yargs) => {
+  yargs.option("title", {
+    describe: "Title of the note",
+    type: "string",
+    demandOption: true,
+  });
+};
+module.exports.handler = (argv) => {
+  const notes = io.getNotes();
+
+  const foundNote = notes.find((note) => note.title === argv.title);
+
+  if (foundNote) {
+    console.log(chalk.blue("=== " + foundNote.title + " ==="));
+    console.log(foundNote.body);
+    console.log(chalk.blue("==================="));
+  } else {
+    console.log(chalk.red("[ERROR] ") + "Note with that title does not exist");
+  }
 };
