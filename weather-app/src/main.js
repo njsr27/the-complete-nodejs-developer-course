88b03a591c41fc6626ac38dbeaaaca946c5fa2d3
Nicolas Saavedra <njsr27@gmail.com>
2021-08-25 03:57:24 -0300

    Chained API calls

--- weather-app/src/main.js
+++ weather-app/src/main.js
@@ -4,11 +4,15 @@ const geocodeAPI = require("./app/services/external/geolocation.js");
 const weatherAPI = require("./app/services/external/weather.js");
 
 geocodeAPI.getLatAndLong("Buenos aires", (error, data) => {
-  console.log(error);
-  console.log(data);
-});
-
-weatherAPI.getWeather(-34.59972, -58.38194, (error, data) => {
-  console.log(error);
-  console.log(data);
+  if (error) {
+    console.log(error)
+  } else {
+    weatherAPI.getWeather(data.lat, data.long, (error, data) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(data);
+      }
+    });
+  }
 });
