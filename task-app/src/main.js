0f8871e95067b500fc8bdae7b009a68150364b64
Nicolas Saavedra <njsr27@gmail.com>
2021-09-03 01:42:35 -0300

    Added mongoose and basic save functionality

--- task-app/src/main.js
+++ task-app/src/main.js
@@ -1,13 +1,39 @@
 "use strict";
 
-const express = require("express");
+// const express = require("express");
+//
+// const app = express();
+//
+// app.get("", (req, res) => {
+//   res.send("It works!");
+// });
+//
+// app.listen(3000, () => {
+//   console.log("Server up and running on port 3000.");
+// });
 
-const app = express();
+const mongoose = require("mongoose");
+const mongoDbConstants = require("./app/constants/mongodb");
 
-app.get("", (req, res) => {
-  res.send("It works!");
-});
+mongoose.connect(
+  mongoDbConstants.CONNECTION_URL + "/task-app",
+  {
+    useNewUrlParser: true,
+    autoIndex: true
+  }
+);
 
-app.listen(3000, () => {
-  console.log("Server up and running on port 3000.");
-});
+const User = mongoose.model("User", {
+  name: {
+    type: String
+  },
+  age: {
+    type: Number
+  }
+})
+
+const me = new User({name: "Lein", age: "30"});
+
+me.save()
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
