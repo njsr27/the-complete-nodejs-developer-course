df831f1a609792efa69db04adee1d92b0d3feda9
Nicolas Saavedra <njsr27@gmail.com>
2021-09-03 01:13:17 -0300

    Fixed comments

--- task-app/src/main.js
+++ task-app/src/main.js
@@ -1,35 +1,13 @@
 "use strict";
 
-// const express = require("express");
-//
-// const app = express();
-//
-// app.get("", (req, res) => {
-//   res.send("It works!");
-// });
-//
-// app.listen(3000, () => {
-//   console.log("Server up and running on port 3000.");
-// });
+const express = require("express");
 
-const mongodb = require("mongodb");
-const mongoClient = mongodb.MongoClient;
-const mongoDbConstants = require("./app/constants/mongodb");
+const app = express();
 
-mongoClient.connect(
-  mongoDbConstants.CONNECTION_URL,
-  {useNewUrlParser: true},
-  (error, client) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const db = client.db(mongoDbConstants.DATABASE_NAME);
-      db.collection("tasks")
-        .deleteOne(
-          {description: "Test task 3"}
-        )
-        .then(result => console.log(result))
-        .catch(error => console.log(error))
-    }
-  }
-)
+app.get("", (req, res) => {
+  res.send("It works!");
+});
+
+app.listen(3000, () => {
+  console.log("Server up and running on port 3000.");
+});
