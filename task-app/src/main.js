2d1f72ac1d8a5802acdcc9faf9309a68428ae1ed
Nicolas Saavedra <njsr27@gmail.com>
2021-09-04 22:04:38 -0300

    Added validation

--- task-app/src/main.js
+++ task-app/src/main.js
@@ -14,6 +14,7 @@
 
 const mongoose = require("mongoose");
 const mongoDbConstants = require("./app/constants/mongodb");
+const validator = require("validator");
 
 mongoose.connect(
   mongoDbConstants.CONNECTION_URL + "/task-app",
@@ -24,16 +25,55 @@ mongoose.connect(
 );
 
 const User = mongoose.model("User", {
-  name: {
-    type: String
-  },
-  age: {
-    type: Number
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate: function (value) {
+        return value > 0;
+      }
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      validate: function (value) {
+        return validator.isEmail(value);
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 7,
+      trim: true
+    }
   }
-})
+)
 
-const me = new User({name: "Lein", age: "30"});
+const Task = mongoose.model("Task", {
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    }
+  }
+)
 
-me.save()
+new User(
+  {
+    name: "Lein Swaisbrick",
+    age: 30,
+    email: "njsr27@gmail.com",
+    password: "asd123%&/"
+  }
+).save()
   .then(result => console.log(result))
   .catch(error => console.log(error));
