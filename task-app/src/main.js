c9fc2ebb8d9ca9925bba5edb6d07c608f556a42f
Nicolas Saavedra <njsr27@gmail.com>
2021-09-07 01:45:47 -0300

    Added tasks endpoint

--- task-app/src/main.js
+++ task-app/src/main.js
@@ -4,12 +4,26 @@ const express = require("express");
 const app = express();
 require("./app/repository/mongodb/taskApp")
 const User = require("./app/models/User")
+const Task = require("./app/models/Task")
 
 app.use(express.json());
 
 app.post("/users", (req, res) => {
   const user = new User(req.body);
   user.save()
+    .then(result => {
+      res.status(201)
+      res.send(result);
+    })
+    .catch(error => {
+      res.status(400);
+      res.send(error);
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+  task.save()
     .then(result => {
       res.status(201)
       res.send(result);
